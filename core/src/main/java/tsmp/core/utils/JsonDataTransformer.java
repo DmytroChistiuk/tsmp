8b93c30ac866fd54eae19d370f9f069117bf8355
Dmytro_Chystiuk <dmytrochistiuk@gmail.com>
2024-11-17 11:54:55 +0200

    Drop not needed files, fix JsonDataTransformer.

--- core/src/main/java/tsmp/core/utils/JsonDataTransformer.java
+++ core/src/main/java/tsmp/core/utils/JsonDataTransformer.java
@@ -1,17 +1,17 @@
 package tsmp.core.utils;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class JsonDataTransformer {
 
     public static <T> List<T> json2Collection(String json, Class<T> type) {
-        return Arrays.asList(new Gson().fromJson(json, type));
+        return new Gson().fromJson(json, TypeToken.getParameterized(List.class, type).getType());
     }
 
-    public static String collection2Json(List collection) {
+    public static String collection2Json(List<?> collection) {
         return new Gson().toJson(collection);
     }
 }
