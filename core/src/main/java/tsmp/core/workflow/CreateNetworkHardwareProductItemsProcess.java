03b04da84db71b64975dedac03bb62a0962b2a15
Dmytro_Chystiuk <dmytrochistiuk@gmail.com>
2024-11-17 13:56:14 +0200

    Added tags, customize metadata editor for asset.

---
+++ core/src/main/java/tsmp/core/workflow/CreateNetworkHardwareProductItemsProcess.java
@@ -0,0 +1,24 @@
+package tsmp.core.workflow;
+
+import com.day.cq.workflow.WorkflowException;
+import com.day.cq.workflow.WorkflowSession;
+import com.day.cq.workflow.exec.WorkItem;
+import com.day.cq.workflow.exec.WorkflowProcess;
+import com.day.cq.workflow.metadata.MetaDataMap;
+import org.apache.sling.api.resource.ResourceResolverFactory;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+@Component(service = WorkflowProcess.class, property = {
+        "process.label=" + "Create Network Hardware Product Items Process"
+})
+public class CreateNetworkHardwareProductItemsProcess implements WorkflowProcess {
+
+    @Reference
+    private ResourceResolverFactory resourceResolverFactory;
+
+    @Override
+    public void execute(WorkItem workItem, WorkflowSession workflowSession, MetaDataMap metaDataMap) throws WorkflowException {
+        //parse and save items to json
+    }
+}
